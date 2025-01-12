be0894bba78b8de2b02d5167db6d9ceb756c7906
wkdnffla3 <wkdnffla3@gmail.com>
2025-01-13 07:32:10 +0900

    Asd

---
+++ java_script/1_basics/3_declaring_variable.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * var 더이상 쓰지 않는다.
+ * 
+ * let
+ */
+
+var name = '코드팩토리' ;
+console.log(name);
+
+
+var age = 32;
+
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+const newJeans = '뉴진스';
+
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언 하는 것.
+ * 
+ * 2) 할당 값을 넣어주는것.
+ */
+
+console.log(name);
